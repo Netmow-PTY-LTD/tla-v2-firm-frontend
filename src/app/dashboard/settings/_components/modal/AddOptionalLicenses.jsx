eecc2cf607e67a5ce2f3ca59077b82550a57f7a5
Tuhin Alom <tuhin.netmow@gmail.com>
2025-09-23 18:20:59 +0600

    certificate and license crud api integrated and company info gett api integrated

--- src/app/dashboard/settings/_components/modal/AddOptionalLicenses.jsx
+++ src/app/dashboard/settings/_components/modal/AddOptionalLicenses.jsx
@@ -1,14 +1,78 @@
 import { Modal } from "@/components/common/components/Modal";
+import { showErrorToast, showSuccessToast } from "@/components/common/toasts";
+import InputCombobox from "@/components/form/ComboboxInput";
+import FormWrapper from "@/components/form/FormWrapper";
 import SelectInput from "@/components/form/SelectInput";
 import TextareaInput from "@/components/form/TextArea";
 import TextInput from "@/components/form/TextInput";
 import { Button } from "@/components/ui/button";
-import React from "react";
+import { useAddFirmWiseLicenseAndCertificationMutation } from "@/store/firmFeatures/certificateLicensesApiService";
+import { useGetFirmUserInfoQuery } from "@/store/firmFeatures/firmAuth/firmAuthApiService";
+import { useGetLawCertificationsListQuery } from "@/store/tlaFeatures/public/publicApiService";
+import Cookies from "js-cookie";
+import React, { useState } from "react";
 
-export default function AddOptionalLicenseModal() {
-  const handleAddOptionalLicense = () => {
-    // Logic to open the modal
-    console.log("Add optional License");
+export default function AddOptionalLicenseModal({
+  defaultValues,
+  schema,
+  refetchLicenses,
+}) {
+  const [isOpen, setIsOpen] = useState(false);
+  const onCancel = () => setIsOpen(!isOpen);
+
+  const token = Cookies.get("token");
+  const { data: currentUser, isLoading: isCurrentUserLoading } =
+    useGetFirmUserInfoQuery(undefined, {
+      skip: !token,
+    });
+
+  const countryId =
+    currentUser?.data?.firmProfile?.contactInfo?.country ||
+    currentUser?.data?.firmProfile?.contactInfo?.country?._id ||
+    ""; // Default to Australia if not available
+  const {
+    data: certificationsList,
+    isLoading: isCertificationsListLoading,
+    isError,
+  } = useGetLawCertificationsListQuery({
+    countryId: countryId,
+    type: "optional",
+    page: 1,
+    limit: 10,
+  });
+  //console.log("Certifications List:", certificationsList);
+
+  const [addLicenseAndCertification] =
+    useAddFirmWiseLicenseAndCertificationMutation();
+  const handleOptionalLicenseSubmit = async (data) => {
+    //console.log("Optional License form submitted:", data);
+    const { certificationId, licenseNumber, validUntil, additionalNote } = data;
+
+    const payload = {
+      certificationId,
+      licenseNumber,
+      validUntil,
+      additionalNote,
+      type: "optional",
+    };
+    console.log("Payload to be sent:", payload);
+
+    try {
+      // Send request to backend
+      const res = await addLicenseAndCertification(payload).unwrap();
+      console.log("Response after adding license and certification:", res);
+      if (res?.success) {
+        // Show success message or toast
+        showSuccessToast(
+          res?.message || "License and certification added successfully"
+        );
+        refetchLicenses();
+        onCancel();
+      }
+    } catch (error) {
+      const errorMessage = error?.data?.message || "An error occurred";
+      showErrorToast(errorMessage);
+    }
   };
   return (
     <Modal
@@ -16,58 +80,64 @@ export default function AddOptionalLicenseModal() {
       description="Add a new license to your firm"
       buttonName="+ Add License"
       width="max-w-[600px]"
+      onOpenChange={setIsOpen}
+      open={isOpen}
     >
       <div className="modal-header">
         <h4 className="text-lg font-semibold ">Add Optional License</h4>
       </div>
-      <div className="grid grid-cols-1 gap-5 mt-6">
-        <SelectInput
-          label="License Type"
-          name="licenseType"
-          placeholder="License Type"
-          options={[
-            { label: "Bar Council of Australia", value: "bar-council" },
-            {
-              label: "Legal Services Commission",
-              value: "legal-services-commission",
-            },
-          ]}
-          triggerClassName="w-full " // set custom width here
-        />
+      <FormWrapper
+        onSubmit={handleOptionalLicenseSubmit}
+        defaultValues={defaultValues}
+        schema={schema}
+      >
+        <div className="grid grid-cols-1 gap-5 mt-6">
+          <InputCombobox
+            label="License Type"
+            name="certificationId"
+            placeholder="Licensing Type"
+            options={
+              certificationsList?.data?.map((cert) => ({
+                value: cert._id,
+                label: cert.certificationName,
+              })) || []
+            }
+            triggerClassName="w-full " // set custom width here
+          />
+          <TextInput
+            label="License Number"
+            name="licenseNumber"
+            placeholder="i.e. ABC1234567"
+          />
 
-        <TextInput
-          label="License Number"
-          name="licenseNumber"
-          placeholder="i.e. ABC1234567"
-        />
+          <TextInput
+            label="Valid Until"
+            name="validUntil"
+            type="date"
+            inputClassName="h-[44px] inline-block  focus-visible:ring-inset w-full"
+          />
 
-        <TextInput
-          label="Valid Until"
-          name="validUntil"
-          type="date"
-          inputClassName="h-[44px] inline-block  focus-visible:ring-inset w-full"
-        />
-
-        <TextareaInput
-          label="Additional Notes"
-          name="notes"
-          placeholder="Any extra notes"
-          className="w-full"
-        />
-      </div>
-      <div className="flex justify-between gap-4 mt-8">
-        <Button type="button" variant={"outline"} className="cursor-pointer">
-          Cancel
-        </Button>
-        <Button
-          type="button"
-          variant={"default"}
-          className="cursor-pointer"
-          onClick={handleAddOptionalLicense}
-        >
-          Add License
-        </Button>
-      </div>
+          <TextareaInput
+            label="Additional Notes"
+            name="additionalNote"
+            placeholder="Any extra notes"
+            className="w-full"
+          />
+        </div>
+        <div className="flex justify-between gap-4 mt-8">
+          <Button
+            type="button"
+            variant={"outline"}
+            className="cursor-pointer"
+            onClick={onCancel}
+          >
+            Cancel
+          </Button>
+          <Button type="submit" variant={"default"} className="cursor-pointer">
+            Add License
+          </Button>
+        </div>
+      </FormWrapper>
     </Modal>
   );
 }
